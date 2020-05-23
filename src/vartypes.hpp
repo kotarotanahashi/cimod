062209f3767092cc1993fc46e7a2d6f63d8a4c72
Kotaro Tanahashi <colon0722@gmail.com>
2020-05-24 01:53:50 +0900

    Update vartypes.hpp

--- src/vartypes.hpp
+++ src/vartypes.hpp
@@ -49,6 +49,7 @@ enum class Vartype
  * @param vartype 
  * @return true or false
  */
+/*
 bool check_vartype
 (
     const int32_t &var,
@@ -86,4 +87,5 @@ bool check_vartype
     }
 }
 }
+*/
 #endif
