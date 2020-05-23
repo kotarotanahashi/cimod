9a40ce1e569ace7212f16a4e46477d54809e4f63
tanahashi-kotaro <tanahashi@r.recruit.co.jp>
2020-05-24 02:08:54 +0900

    update vartypes

--- src/vartypes.hpp
+++ src/vartypes.hpp
@@ -49,6 +49,7 @@ enum class Vartype
  * @param vartype 
  * @return true or false
  */
+/*
 bool check_vartype
 (
     const int32_t &var,
@@ -83,6 +84,6 @@ bool check_vartype
     {
         std::cerr << "Unknown variable type." << std::endl;
     }
-};
+};*/
 }
 #endif
